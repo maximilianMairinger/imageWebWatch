14552bfab17ae10dd901377a5d0471304ee29f5a
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2024-06-28 10:26:23 +0200

    ctp init app

---
+++ app/res/lang/de.ts
@@ -0,0 +1,14 @@
+export default {
+  appName: {
+    short: "imageWebWatch",
+    long: "Image web watch"
+  },
+  links: {
+    contact: "Kontakt",
+    imprint: "Impressum",
+    privacy: "Datenschutz",
+    terms: "AGB"
+  },
+  confirm: "Bestätigen",
+  copiedFeedback: "Kopiert!",
+}
---
+++ app/res/lang/en.ts
@@ -0,0 +1,14 @@
+export default {
+  appName: {
+    short: "imageWebWatch",
+    long: "Image web watch"
+  },
+  links: {
+    contact: "Contact",
+    imprint: "Imprint",
+    privacy: "Privacy",
+    terms: "Terms"
+  },
+  confirm: "Confirm",
+  copiedFeedback: "Copied!",
+}
